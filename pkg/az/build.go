5ecdecbf5ea0100f835ca296b76848d709ad2af4
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-09-03 16:46:50 -0500

    Install the az cli

--- pkg/az/build.go
+++ pkg/az/build.go
@@ -1,20 +1,12 @@
 package az
 
-// This is an example. Replace thie following with whatever steps are needed to
-// install required components into
-// const dockerfileLines = `RUN apt-get update && \
-// apt-get install gnupg apt-transport-https lsb-release software-properties-common -y && \
-// echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ stretch main" | \
-//    tee /etc/apt/sources.list.d/azure-cli.list && \
-// apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
-// 	--keyserver packages.microsoft.com \
-// 	--recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF && \
-// apt-get update && apt-get install azure-cli
-// `
+import "fmt"
 
-// Build will generate the necessary Dockerfile lines
-// for an invocation image using this mixin
+// Build installs the az cli
 func (m *Mixin) Build() error {
-	//fmt.Fprintf(m.Out, dockerfileLines)
+	fmt.Fprintln(m.Out, `RUN apt-get update && apt-get install -y apt-transport-https lsb-release gnupg curl`)
+	fmt.Fprintln(m.Out, `RUN curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.asc.gpg`)
+	fmt.Fprintln(m.Out, `RUN echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/azure-cli.list`)
+	fmt.Fprintln(m.Out, `RUN apt-get update && apt-get install -y azure-cli`)
 	return nil
 }

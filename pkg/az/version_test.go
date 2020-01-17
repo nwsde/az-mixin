64998fea5079cd0d5bf329bfe26861a66617bc3a
Vaughn Dice <vadice@microsoft.com>
2020-01-17 15:54:57 -0500

    chore(*): cdn and mixin author updates
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- pkg/az/version_test.go
+++ pkg/az/version_test.go
@@ -22,7 +22,7 @@ func TestPrintVersion(t *testing.T) {
 	m.PrintVersion(opts)
 
 	gotOutput := m.TestContext.GetOutput()
-	wantOutput := "az v1.2.3 (abc123) by DeisLabs"
+	wantOutput := "az v1.2.3 (abc123) by Porter Authors"
 	if !strings.Contains(gotOutput, wantOutput) {
 		t.Fatalf("invalid output:\nWANT:\t%q\nGOT:\t%q\n", wantOutput, gotOutput)
 	}
@@ -45,7 +45,7 @@ func TestPrintJsonVersion(t *testing.T) {
   "name": "az",
   "version": "v1.2.3",
   "commit": "abc123",
-  "author": "DeisLabs"
+  "author": "Porter Authors"
 }
 `
 	if !strings.Contains(gotOutput, wantOutput) {

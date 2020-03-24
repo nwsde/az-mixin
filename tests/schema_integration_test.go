31074a21ab5654ea06da87dfc5729db4a10232cc
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2020-03-24 10:39:04 -0500

    Test that schema was packed into binary
    Add a test that checked that the schema was packed into the binary
    properly. This may not always fail on a dev machine but will
    consistently fail on CI when we forgot to add
    `//go:generate packr2` or to run `generate`.

---
+++ tests/schema_integration_test.go
@@ -0,0 +1,33 @@
+package tests
+
+import (
+	"bytes"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Add a test that checked that the schema was packed into the binary
+// properly. Requires a make clean xbuild-all first.
+func TestSchema(t *testing.T) {
+	schemaBackup := "../pkg/az/schema/schema.json.bak"
+	schemaPath := "../pkg/az/schema/schema.json"
+	defer os.Rename(schemaBackup, schemaPath)
+	err := os.Rename(schemaPath, schemaBackup)
+	require.NoError(t, err, "failed to sabotage the schema.json")
+
+	output := &bytes.Buffer{}
+	cmd := exec.Command("az", "schema")
+	cmd.Path =  "../bin/mixins/az/az"
+	cmd.Stdout = output
+	cmd.Stderr = output
+
+	err = cmd.Start()
+	require.NoError(t, err, "failed to start the az schema command")
+
+	err = cmd.Wait()
+	t.Log(output)
+	require.NoError(t, err, "az schema failed")
+}

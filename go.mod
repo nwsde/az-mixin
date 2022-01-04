e922f42996a202d22064f7063332eae045c4ae47
Carolyn Van Slyck <me@carolynvanslyck.com>
2022-01-04 13:09:35 -0600

    Add idemponent resource group command
    The group command makes it simpler to ensure a resource group
    exists, and that it has been cleaned up when the bundle is uninstalled.
    
    When used in any action other than uninstall, the mixin will ensure that the resource group exists.
    ```yaml
    install:
      - az:
          description: "Ensure my group exists"
          group:
            name: mygroup
            location: westus
    ```
    
    When used in the uninstall action, the mixin ensures that the resource group is deleted.
    ```yaml
    uninstall:
      - az:
          description: "Cleanup my group"
          group:
            name: mygroup
    ```
    
    I have also added the ignoreError functionality from the exec mixin so
    that custom user commands can handle errors.
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

--- go.mod
+++ go.mod
@@ -8,8 +8,9 @@ replace (
 )
 
 require (
-	get.porter.sh/porter v1.0.0-alpha.5
+	get.porter.sh/porter v1.0.0-alpha.7
 	github.com/ghodss/yaml v1.0.0
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.1.3
 	github.com/stretchr/testify v1.7.0
 	github.com/xeipuuv/gojsonschema v1.2.0
---
+++ pkg/az/group.go
@@ -0,0 +1,75 @@
+package az
+
+import (
+	"strings"
+
+	"get.porter.sh/porter/pkg/context"
+	"get.porter.sh/porter/pkg/exec/builder"
+)
+
+var (
+	_ TypedCommand             = &GroupCommand{}
+	_ builder.HasErrorHandling = &GroupCommand{}
+)
+
+// GroupCommand ensures that a group exist or not
+type GroupCommand struct {
+	action   string
+	Name     string `yaml:"name"`
+	Location string `yaml:"location"`
+}
+
+func (c *GroupCommand) HandleError(cxt *context.Context, err builder.ExitError, stdout string, stderr string) error {
+	switch c.action {
+	case "uninstall":
+		// It's okay if we try to delete the resource group and it's already gone
+		if strings.Contains(stderr, "ResourceGroupNotFound") {
+			return nil
+		}
+	default:
+		// NOTE: We are treating "already exists" as an okay state and reusing the existing group,
+		// but the user could have requested a different location.
+		// We may want a feature that recreates the group to get it into the correct location.
+		if strings.Contains(stderr, "Resource group already exists") {
+			return nil
+		}
+	}
+
+	return err
+}
+
+func (c *GroupCommand) GetWorkingDir() string {
+	return ""
+}
+
+func (c *GroupCommand) SetAction(action string) {
+	c.action = action
+}
+
+func (c *GroupCommand) GetCommand() string {
+	return "az"
+}
+
+func (c *GroupCommand) GetArguments() []string {
+	switch c.action {
+	case "uninstall":
+		return []string{"group", "delete", "--yes"}
+	default:
+		return []string{"group", "create"}
+	}
+}
+
+func (c *GroupCommand) GetFlags() builder.Flags {
+	var flags builder.Flags
+	flags = append(flags, builder.NewFlag("name", c.Name))
+
+	if c.action != "uninstall" {
+		flags = append(flags, builder.NewFlag("location", c.Location))
+	}
+
+	return flags
+}
+
+func (c *GroupCommand) SuppressesOutput() bool {
+	return false
+}
